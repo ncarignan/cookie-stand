24cc9979fecac26e8e5c3178b6ec75941b7e447d
Nicholas Carignan <nick.carignan@sbcglobal.net>
2017-09-27 16:31:11 -0700

    fin table/input, working on styling

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ var standArray = [];
 var totalHourArray = [];
 var storeForm = document.getElementById('storeForm');
 var standsTable = document.getElementById('standsTable');
+var allLocationsRef = [];
 
 
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 function CookieStand(name, minCustomers, maxCustomers, aveCookies){
   this.name = name;
+  allLocationsRef.push(this.name);
   this.minCustomers = minCustomers;
   this.maxCustomers = maxCustomers;
   this.aveCookies = aveCookies;
@@ -21,7 +23,7 @@ function CookieStand(name, minCustomers, maxCustomers, aveCookies){
   this.sumCookieSold = 0;
   standArray.push(this);
   this.randRate();
-  this.hourlyRateTable();
+  this.createHourlyRateTable();
 }
 
 
@@ -36,15 +38,18 @@ CookieStand.prototype.randRate = function(){
   this.cookieSoldArray.push(this.cookieSoldTotal);
 };
 
-CookieStand.prototype.hourlyRateTable = function(){
+CookieStand.prototype.createHourlyRateTable = function(){
   this.trEl = document.createElement('tr');
   standsTable.appendChild(this.trEl);
-  this.cookieSoldArray.unshift(this.name);
-  console.log(this.cookieSoldArray);
+  var locCSArray = this.cookieSoldArray.slice(0);
+  console.log(locCSArray);
+  // this.cookieSoldArray.unshift(this.name);
+  locCSArray.unshift(this.name);
+  // console.log(this.cookieSoldArray);
 
-  for(var i = 0; i < (this.cookieSoldArray.length); i++){
+  for(var i = 0; i < locCSArray.length; i++){
     this.tdEl = document.createElement('td');
-    this.tdEl.textContent = this.cookieSoldArray[i];
+    this.tdEl.textContent = locCSArray[i];
     this.trEl.appendChild(this.tdEl);
   }
 };
@@ -71,6 +76,15 @@ new CookieStand('Alki', 2, 16, 4.6);
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //new Location function
 //function to re-render table
+function renderTable(){
+  standsTable.innerHTML = null;
+  makeHeaderRow();
+  for(var i in standArray){
+    standArray[i].createHourlyRateTable();
+  }
+  sumColumns();
+}
+
 
 function handleLocationSubmit(event) {
   event.preventDefault();
@@ -82,14 +96,24 @@ function handleLocationSubmit(event) {
   var maxCustomers = parseInt(event.target.maxCustomers.value);
   var aveCookies = parseInt(event.target.aveCookies.value);
 
+  if(allLocationsRef.indexOf(name) > -1){
+    standArray[allLocationsRef.indexOf(name)].minCustomers = minCustomers;
+    standArray[allLocationsRef.indexOf(name)].maxCustomers = maxCustomers;
+    standArray[allLocationsRef.indexOf(name)].aveCookies = aveCookies;
+    standArray[allLocationsRef.indexOf(name)].cookieSoldArray = [];
+    standArray[allLocationsRef.indexOf(name)].randRate();
+    renderTable();
+    return;
+  }
+
   var newCookieStand = new CookieStand(name,minCustomers,maxCustomers, aveCookies);
+  console.log(newCookieStand);
 
   event.target.name.value = null;
   event.target.minCustomers.value = null;
   event.target.maxCustomers.value = null;
   event.target.aveCookies.value = null;
-  standArray.push(newCookieStand);
-  //render table
+  renderTable();
 }
 
 storeForm.addEventListener('submit', handleLocationSubmit);
@@ -97,7 +121,8 @@ storeForm.addEventListener('submit', handleLocationSubmit);
 
 //end table construction adds
 var sumColumns = function(){
-  for(var j = 0; j < standArray[0].cookieSoldArray.length; j++){
+  totalHourArray = [];
+  for(var j = -1; j < standArray[0].cookieSoldArray.length ; j++){
     var hourTotal = 0;
     for(var k in standArray){
       hourTotal = hourTotal + standArray[k].cookieSoldArray[j];
